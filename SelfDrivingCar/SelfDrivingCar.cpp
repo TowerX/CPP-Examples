6a510fe6d85076857b9b4085a0a3d45e19a13e6d
TowerX <jptb86@gmail.com>
2020-02-23 11:20:12 -0500

    Add project files.

---
+++ SelfDrivingCar/SelfDrivingCar.cpp
@@ -0,0 +1,12 @@
+/*
+Author: Jay Torres
+Date: February 23, 2020
+Purpose: Simple Geometric Calculator > Calculate the area of given sides.
+FileName: SelfDrivingCar.cpp : This file contains the 'main' function. Program execution begins and ends there.
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
