2d0f5bdb71eaed20d3e24ae659888bbbfd650410
TowerX <jptb86@gmail.com>
2020-02-23 15:22:46 -0500

    Add variables

--- SelfDrivingCar/SelfDrivingCar.cpp
+++ SelfDrivingCar/SelfDrivingCar.cpp
@@ -5,8 +5,16 @@ Purpose: Create a program that simulates the decisions needed when a self-drivin
 FileName: SelfDrivingCar.cpp : This file contains the 'main' function. Program execution begins and ends there.
 */
 #include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string lightColor = "";
+	string traffic = "";
+	string turnSignal = "";
+
+	return 0;
 }
+
