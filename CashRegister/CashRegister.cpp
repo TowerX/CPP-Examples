18ed335383f62c01d1ba634ab5a8164193acedd7
TowerX <jptb86@gmail.com>
2020-03-04 14:21:09 -0500

    Add variables

--- CashRegister/CashRegister.cpp
+++ CashRegister/CashRegister.cpp
@@ -2,10 +2,33 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// 7% tax Rate
+	const float taxRate = 7;
+
+	// Array to hold values
+	float itemAmount[5];
+	float itemTax[5];
+	float itemSubtotal[5];
+
+	// Initializing variables
+	float itemTotal = 0;
+	float taxTotal = 0;
+	float totalDue = 0;
+
+	// Calculation loop
+	// Set decimals to display (2)
+	cout.precision(2);
+	cout.setf(ios::fixed);
+
+	for (int i = 0; i < 5; i++) {
+
+	}
+
+	return 0;
 }
 
 
