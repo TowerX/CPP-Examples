56d7a4dcadbb314dd6e2aba596cb3fd9764e84cb
TowerX <jptb86@gmail.com>
2020-03-08 23:33:07 -0400

    Add logic to sort and search arrays

--- SimpleContactManager/SimpleContactManager.cpp
+++ SimpleContactManager/SimpleContactManager.cpp
@@ -17,30 +17,63 @@ int main()
 	const int SIZE = 3;
 	string name[SIZE] = { "" };
 	string phone[SIZE] = { "" };
+	string searchInput;
 	
-	// Fill the array with data
+	// Header information
 	cout << "Please enter 3 contact names and phone numbers" << endl;
 	cout << "--------------------------------------------------------------" << endl;
-
+	// Fill the array with data
 	for (int i = 0; i < SIZE; i++) {
 		cout << "Enter the contact name:" << endl;
 		cin >> name[i];
 		cout << "Enter the phone number for:" << name[i] << endl;
 		cin >> phone[i];
 	}
+
+	// Sort the array after user input based on the phone number
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE - 1; j++) {
+			if (phone[j] > phone[j + 1]) {
+				// Declare string variable temp to hold the value of the array and swap its values as reference.
+				string temp = phone[j];
+				phone[j] = phone[j + 1];
+				phone[j + 1] = temp;
+			}
+		}
+	}
+
+	// Print the content of the array
+	for (int i = 0; i < SIZE; i++) {
+		cout << setw(2) << i + 1 << "." << "Name: " << setw(20) << name[i] << " | " << "Number: " << setw(15) << phone[i] << endl;
+	}
+	
+	// Search the array based on user input and store the value on searchInput variable.
+	cout << "Enter a name to search" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cin >> searchInput;
+	// Search the actual array with provided input.
+	for (int i = 0; i < SIZE; i++) {
+		if (name[i] == searchInput) {
+			cout << "Contact Found! The information is below" << endl;
+			cout << "--------------------------------------------------------------" << endl;
+			cout << setw(2) << "Name: " << setw(20) << name[i] << " | " << "Number: " << setw(15) << phone[i] << endl;
+		}
+		else {
+			cout << "Contact not found!." << endl;
+		}
+	}
+
+	// Input name
 	
 	return 0;
 }
 
 
 /*
-1 - The program is to store individual names and associated telephone numbers in parallel arrays.
-2 - The program should input from the user 10 people’s names and their telephone numbers and add the data to the parallel arrays.
 3 - When data input is finished, the program should sort both data arrays in ascending order based on telephone numbers maintaining the associations between namesand telephone numbers.
-4 - The program will print the list of usersand their telephone numbers after sorting.
 5 - After printing the list, the program will ask for a name to search for.
 6 - The user will then enter a name.
-7 - The program will then search for the nameand print the nameand the associated telephone number for that person.
+7 - The program will then search for the name and print the nameand the associated telephone number for that person.
 8 - If the person is not in the array, the program will print that the user cannot be found.
 */
 
