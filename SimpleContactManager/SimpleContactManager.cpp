f4559f85e3b9c2f60ae374de179d0fa617001707
TowerX <jptb86@gmail.com>
2020-03-08 22:52:09 -0400

    Add variables

--- SimpleContactManager/SimpleContactManager.cpp
+++ SimpleContactManager/SimpleContactManager.cpp
@@ -6,8 +6,31 @@ FileName: SimpleContactManager.cpp
 */
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Declare name and phone array variables with a size of variable SIZE in this case, 10
+	const int SIZE = 10;
+	string name[SIZE];
+	string phone[SIZE];
+
+
+
+
+
+
 }
+
+
+/*
+1 - The program is to store individual names and associated telephone numbers in parallel arrays.
+2 - The program should input from the user 10 people’s names and their telephone numbers and add the data to the parallel arrays.
+3 - When data input is finished, the program should sort both data arrays in ascending order based on telephone numbers maintaining the associations between namesand telephone numbers.
+4 - The program will print the list of usersand their telephone numbers after sorting.
+5 - After printing the list, the program will ask for a name to search for.
+6 - The user will then enter a name.
+7 - The program will then search for the nameand print the nameand the associated telephone number for that person.
+8 - If the person is not in the array, the program will print that the user cannot be found.
+*/
+
