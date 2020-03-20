7704dec7c2a1626d401f40dff4ca51e512e424d4
TowerX <jptb86@gmail.com>
2020-03-20 17:13:18 -0400

    Program Completed: Contact Manager Program

--- ContactManagerProgram/ContactManagerProgram.cpp
+++ ContactManagerProgram/ContactManagerProgram.cpp
@@ -15,13 +15,16 @@ using namespace std;
 
 // Constant variable holding the array size for easy access to modifications.
 const int SIZE = 3;
+// Array declaration and initialized as an empty array
 string firstName[SIZE] = { "" };
 string lastName[SIZE] = { "" };
 string phoneNumber[SIZE] = { "" };
 string emailAddress[SIZE] = { "" };
+// Variable holding the search input data.
 string searchInput = "";
 
 // Functions
+////////////
 
 // Get input from the user
 void inputData() {
@@ -34,12 +37,15 @@ void inputData() {
 		cin >> firstName[index];
 		cout << "Enter " << firstName[index] << ", last name:" << endl;
 		cin >> lastName[index];
+		cout << "Enter the phone number for: " << firstName[index] << endl;
+		cin >> phoneNumber[index];
 		cout << "Enter " << firstName[index] << ", " << lastName[index] << " email address:" << endl;
 		cin >> emailAddress[index];
 	}
 }
 
 // Perform a search based on user input
+// Search by first name, last name and, phone number. Email is not searchable on this program.
 void searchData() {
 	// Get user input and store the value on searchInput variable.
 	cout << "Enter a name to search" << endl;
@@ -51,8 +57,9 @@ void searchData() {
 		if (firstName[index] == searchInput || lastName[index] == searchInput || phoneNumber[index] == searchInput) {
 			cout << "\nContact Found! The information is below" << endl;
 			cout << "--------------------------------------------------------------" << endl;
-			cout << setw(2) << "FirstName: " << setw(20) << firstName[index] << " | " << "LastName: " << setw(20) << firstName[index] << " | " << "Phone Number: " << setw(15) << phoneNumber[index] << " | " << "FirstName: " << setw(20) << firstName[index] << " | " << "EmailAddress: " << emailAddress[index] << endl;
+			cout << setw(2) << "FirstName: " << setw(15) << firstName[index] << " | " << "LastName: " << setw(10) << lastName[index] << " | " << "PhoneNumber: " << setw(13) << phoneNumber[index] << " | " << "EmailAddress: " << setw(15) << emailAddress[index] << endl;
 			cout << "\n\n";
+			break;
 		}
 		else {
 			// If contact is not found
@@ -68,12 +75,12 @@ void printData() {
 	cout << "\nPhone Contacts" << endl;
 	cout << "--------------------------------------------------------------" << endl;
 	for (int index = 0; index < SIZE; index++) {
-		cout << setw(2) << "FirstName: " << setw(20) << firstName[index] << " | " << "LastName: " << setw(20) << firstName[index] << " | " << "Phone Number: " << setw(15) << phoneNumber[index] << " | " << "FirstName: " << setw(20) << firstName[index] << " | " << "EmailAddress: " << emailAddress[index] << endl;
+		cout << setw(2) << "FirstName: " << setw(15) << firstName[index] << " | " << "LastName: " << setw(10) << lastName[index] << " | " << "PhoneNumber: " << setw(13) << phoneNumber[index] << " | " << "EmailAddress: " << setw(15) << emailAddress[index] << endl;
 	}
 	cout << "\n\n";
 }
 
-// Descending sort function using the bubble sort technique.
+// Descending sort function using the bubble sort technique. Based on phones array.
 void sortDescending(string fNames[], string lNames[], string emails[], string phones[], int size) {
 	int maxElement;
 	int index;
@@ -89,7 +96,7 @@ void sortDescending(string fNames[], string lNames[], string emails[], string ph
 	}
 }
 
-// Ascending sort function using the bubble sort technique.
+// Ascending sort function using the bubble sort technique. Based on phones array.
 void sortAscending(string fNames[], string lNames[], string emails[], string phones[], int size) {
 	int maxElement;
 	int index;
@@ -112,6 +119,7 @@ void swap(string a, string b) {
 	a = b;
 	b = temp;
 }
+////////////////
 // End Functions
 
 int main()
@@ -128,7 +136,7 @@ int main()
 		cout << "Press 4 to Print data:" << endl;
 		cout << "Press 5 to Search data:" << endl;
 		cout << "Press 6 to Exit the program:" << endl;
-
+		// Prompt the user for the menu choice based on the menu option numbers.
 		cin >> menuChoice;
 		cout << "\n\n" << endl;
 
@@ -143,7 +151,7 @@ int main()
 			cout << "\nData sorted in Ascending order" << endl;
 			cout << "--------------------------------------------------------------" << endl;
 			cout << "\n\n";
-			sortAscending(firstName, lastName, phoneNumber, emailAddress, SIZE);
+			sortAscending(firstName, lastName, emailAddress, phoneNumber, SIZE);
 			printData();
 			break;
 		case 3:
@@ -151,7 +159,7 @@ int main()
 			cout << "\nData sorted in Descending order" << endl;
 			cout << "--------------------------------------------------------------" << endl;
 			cout << "\n\n";
-			sortDescending(firstName, lastName, phoneNumber, emailAddress, SIZE);
+			sortDescending(firstName, lastName, emailAddress, phoneNumber, SIZE);
 			printData();
 			break;
 		case 4:
@@ -163,7 +171,7 @@ int main()
 			searchData();
 			break;
 		case 6:
-			// Exit program
+			// Exit program by returning false to the while loop
 			return 0;
 			break;
 		default:
